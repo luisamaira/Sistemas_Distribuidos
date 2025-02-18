c89f33507b83fbfa6564679389a142b5c275c7e6
Luisa Maira <102473816+luisamaira@users.noreply.github.com>
2025-02-17 23:11:50 -0300

    Add files via upload

---
+++ Trabalho_Final/Application/GerenciamentoProjetosApplication.java
@@ -0,0 +1,13 @@
+package br.luisa.GerenciamentoProjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoProjetosApplication.class, args);
+	}
+
+}
